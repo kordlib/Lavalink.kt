f0580b727571a7f2374b9f398e67c0e3a9ef0fe3
schlaubi <michael@rittmeister.in>
2020-11-04 15:36:26 +0100

    Add pom metadata to publishing to sync artifacts with maven-central

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,33 @@ tasks {
                 from(javaComponent)
                 artifact(sourcesJar)
                 artifact(javadocJar)
+
+                pom {
+                    name.set(project.name)
+                    description.set("Extension of the official LavaLink-Client to work with Kord")
+                    url.set("https://github.com/DRSchlaubi/lavakord")
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/DRSchlaubi/Lavakord/blob/master/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            name.set("Michael Rittmeister")
+                            email.set("mail@schlaubi.me")
+                            organizationUrl.set("https://michael.rittmeister.in")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:https://github.com/DRSchlaubi/lavakord.git")
+                        developerConnection.set("scm:git:https://github.com/DRSchlaubi/lavakord.git")
+                        url.set("https://github.com/DRSchlaubi/lavakord")
+                    }
+                }
             }
         }
     }
