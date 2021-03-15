61553911ddb4d9d5e7650eb640ffc230e3a425ca
Michael Rittmeister <michael@rittmeister.in>
2021-03-15 12:59:57 +0100

    Improve java interop

--- java/src/jvmMain/kotlin/dev/kord/x/lavalink/interop/CoroutineUtil.kt
+++ java/src/jvmMain/kotlin/dev/kord/x/lavalink/interop/CoroutineUtil.kt
@@ -1,12 +1,11 @@
 package dev.kord.x.lavalink.interop
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.future.future
 import java.util.concurrent.CompletableFuture
 
 internal fun <T> CoroutineScope.supply(supplier: suspend CoroutineScope.() -> T) =
-    async(block = supplier).asCompletableFuture()
+    future(block = supplier)
 
 internal fun CoroutineScope.run(supplier: suspend CoroutineScope.() -> Unit): CompletableFuture<Void> =
     supply(supplier).thenApply { null }
