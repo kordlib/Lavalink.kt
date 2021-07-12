265ae43c59fbd22d8bdeb7fc9a7eb209b5876330
Michael Rittmeister <michael@rittmeister.in>
2021-07-12 03:34:03 +0200

    GH actions got mad

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "lavakord"
-include("example")
-include("kord")
+//include("example")
+include("kord") // GitHub Actions gets mad about this and I can't reproduce this locally
 //include("jsExample") // kotlinx-nodejs is unavailable: https://github.com/Kotlin/kotlinx-nodejs/issues/16
 include("core")
 include("java")
