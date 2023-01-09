d5c2433678f7975030555480530f98d5827f0166
Michael Rittmeister <michael@rittmeister.in>
2023-01-09 16:47:12 +0100

    Fix gradle build issue

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,12 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     groovy
     `kotlin-dsl`
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
 }
-
-group = "me.schlaubi"
-version = "3.0.1"
 
 repositories {
     mavenCentral()
@@ -16,11 +11,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugina-api"))
-    implementation(kotlin("gradle-plugin"))
-    implementation(kotlin("serialization"))
+    val kotlinVersion = "1.8.0"
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+    implementation(kotlin("serialization", kotlinVersion))
+    implementation(kotlin("gradle-plugin-api", kotlinVersion))
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.7.20")
-    implementation(kotlin("gradle-plugin-api"))
     implementation(gradleApi())
     implementation(localGroovy())
 }
