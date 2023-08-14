e239584ff64a0faa21d15e8526d0ac0d01943bea
Michael Rittmeister <michael@rittmeister.in>
2023-08-14 16:20:05 +0200

    Fix compilation

--- jda/build.gradle.kts
+++ jda/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api(projects.core)
     api(libs.kotlinlogging)
     api(libs.kotlinx.coroutines.jdk8)
-    implementation("net.dv8tion:JDA:5.0.0-beta.12") {
+    api("net.dv8tion:JDA:5.0.0-beta.13") {
         exclude(module = "opus-java")
     }
 }
